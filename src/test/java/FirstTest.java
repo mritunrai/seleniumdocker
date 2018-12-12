f1c4d264efdf7c21700a936da561ed4897d27416
amartans <amartans@thoughtworks.com>
2018-12-12 21:12:42 +0530

    [Amar]Modifed Test classes

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,32 +1,30 @@
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class FirstTest extends BaseTest
 {
     @Test
-    public void GOOGLE1() throws Exception {
-        System.out.println("Google1 Test Started! " + Thread.currentThread().getId());
-        getDriver().navigate().to("http://www.google.com");
-        System.out.println("Google1 Test's Page title is: " + getDriver().getTitle() +" "+ Thread.currentThread().getId());
-        Assert.assertEquals(getDriver().getTitle(), "Google");
-        System.out.println("Google1 Test Ended! " + Thread.currentThread().getId());
+    public void FacebookTest() throws Exception {
+        Reporter.log(String.format("Facebook Test Started %s",Thread.currentThread().getId()),true);
+        getDriver().navigate().to("http://www.facebook.com");
+        Assert.assertEquals(getDriver().getTitle(), "Facebook – log in or sign up");
+        Reporter.log(String.format("Facebook Test Ended %s",Thread.currentThread().getId()),true);
     }
 
     @Test
-    public void GOOGLE2() throws Exception {
-        System.out.println("Google2 Test Started! " + Thread.currentThread().getId());
-        getDriver().navigate().to("http://www.google.com");
-        System.out.println("Google2 Test's Page title is: " + getDriver().getTitle() +" "+ Thread.currentThread().getId());
-        Assert.assertEquals(getDriver().getTitle(), "Google");
-        System.out.println("Google2 Test Ended! " + Thread.currentThread().getId());
+    public void GmailTest() throws Exception {
+        Reporter.log(String.format("Gmail Test Started %s",Thread.currentThread().getId()),true);
+        getDriver().navigate().to("http://www.gmail.com");
+        Assert.assertEquals(getDriver().getTitle(), "Gmail");
+        Reporter.log(String.format("Gmail Test Ended %s",Thread.currentThread().getId()),true);
     }
 
     @Test
-    public void GOOGLE3() throws Exception {
-        System.out.println("Google3 Test Started! " + Thread.currentThread().getId());
-        getDriver().navigate().to("http://www.google.com");
-        System.out.println("Google3 Test's Page title is: " + getDriver().getTitle() +" "+ Thread.currentThread().getId());
-        Assert.assertEquals(getDriver().getTitle(), "Google");
-        System.out.println("Google3 Test Ended! " + Thread.currentThread().getId());
+    public void InstagramTest() throws Exception {
+        Reporter.log(String.format("Instagram Test Started %s",Thread.currentThread().getId()),true);
+        getDriver().navigate().to("https://www.instagram.com/");
+        Assert.assertEquals(getDriver().getTitle(), "Instagram");
+        Reporter.log(String.format("Instagram Test Ended %s",Thread.currentThread().getId()),true);
     }
 }
